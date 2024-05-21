1c34aadfa980b399b1b1b488581ec956709cca04
Fooxeek <foxeek02@gmail.com>
2024-05-21 13:07:44 +0200

    chore: Remove commented out code in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,39 +18,3 @@ export default function Hero({ userId }) {
     </div>
   );
 }
-
-// import React, { useState, useEffect } from "react";
-
-// export default function Hero({ userId }) {
-//   const [user, setUser] = useState(null);
-
-//   const fetchUserData = async () => {
-//     const response = await fetch(`http://localhost:3000/user/${userId}`);
-//     if (!response.ok) {
-//       // Gérer les erreurs, par exemple un 404 ou autre
-//       console.error("Erreur lors de la récupération des données utilisateur");
-//       setUser({ userInfos: { firstName: "Guest" } });
-//     } else {
-//       const userData = await response.json();
-//       setUser(userData.data);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchUserData();
-//   }, []);
-
-//   return (
-//     <div className="hero">
-//       <h1 className="text-4xl font-bold">
-//         Bonjour{" "}
-//         <span className="text-red-600">
-//           {user ? user.userInfos.firstName : "Guest"}
-//         </span>
-//       </h1>
-//       <p className="pt-5">
-//         Félicitations ! Vous avez explosé vos objectifs hier 👏
-//       </p>
-//     </div>
-//   );
-// }
